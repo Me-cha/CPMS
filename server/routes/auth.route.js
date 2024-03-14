8e294fe1b62a1abad5a7fa9c5c06eec50c84486f
Me-cha <127575583+Me-cha@users.noreply.github.com>
2024-03-14 11:00:29 +0530

    working model 14-03-2024

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -6,7 +6,7 @@ const auth = require("../middlewares/auth.middleware");
 const { signup, login, co_signup } = require("../controllers/auth.controller");
 
 router.post("/signup", signup);
-router.post("/login", auth, login);
+router.post("/login", login);
 
 router.post("/coordinator/signup", co_signup);
 
